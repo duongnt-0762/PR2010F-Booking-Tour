2c95c95a0e985696e28155c27be3319e983b6f40
hoanghq90 <hoangts140190@gmail.com>
2021-01-07 09:04:40 +0700

    hoang

--- app/assets/images/application.js
+++ app/assets/images/application.js
@@ -7,15 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import '../stylesheets/application'
-import "@fortawesome/fontawesome-free/js/all";
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 require("jquery")
-require('admin-lte')
 import "bootstrap"
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-});
