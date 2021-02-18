6db4de295e1f9318ce3c1bf2ef4e09fc9dc35ccf
Jeremie <wucututa@gmail.com>
2021-02-18 11:28:12 +0700

    loginfb-modal-i18n

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Air bnb!"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
   def show
     @user =  User.find_by id: params[:id]
     if current_user.admin?
@@ -9,4 +24,9 @@ class UsersController < ApplicationController
       redirect_to root_url
     end
   end
+
+  private
+ def user_params
+   params.require(:user).permit :name, :email, :password,:password_confirmation
+ end
 end
